bdc7a0bbdc49b8837f5826f8a45218417ed1121a
Mateusz Bartczak <mati.2206@wp.pl>
2023-03-06 13:12:11 +0100

    rzeczy

--- server/public/api/photos/index.php
+++ server/public/api/photos/index.php
@@ -1,4 +1,1 @@
-<?php
-header("Content-Type: application/json; charset=UTF-8");
-echo json_encode(array_slice(scandir(dirname(__FILE__,3)."/photos"), 2));
-?>
+<?=json_encode(array_slice(scandir(dirname(__FILE__,3)."/photos"), 2));?>
