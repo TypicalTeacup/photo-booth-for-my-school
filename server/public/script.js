b097374a0444be53823d282c3633cebe544c1a66
TypicalTeacup <pskirel@wp.pl>
2023-03-08 20:35:39 +0000

    jakieś komentarze w js

--- server/public/script.js
+++ server/public/script.js
@@ -2,10 +2,13 @@ import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@6.6.2/dist/fuse.esm.js";
 const searchbar = document.querySelector("#search");
 const photosDiv = document.querySelector("#photos");
 
+// silnik wyszukiwania
 let fuse = new Fuse();
+
 let allPhotos;
 let photos;
 
+// pobiera listę zdjęć z serwera, potem wyświetla
 const fetchPhotos = () => {
     fetch(`http://${window.location.host}/api/photos`)
         .then((r) => r.json())
@@ -23,17 +26,24 @@ document.querySelector("#refresh").addEventListener("click", () => {
     searchbar.value = "";
 });
 
+/*
+przykładowy element:
+<a class="photolink" href="photos/zdj.jpg" target="_blank">
+    <img loading="lazy" src="photos/zdj.jpg" />
+</a>
+*/
 const renderPhotos = () => {
     photosDiv.innerHTML = "";
     photos.forEach((photo) => {
         const link = document.createElement("a");
         link.classList.add("photolink");
         link.href = `photos/${photo}`;
-        link.target = "_blank"
+        link.target = "_blank";
 
         const image = document.createElement("img");
-        image.loading = "lazy";
         image.src = `photos/${photo}`;
+        // żeby się nie ładowało jak nie trzeba
+        image.loading = "lazy";
         link.appendChild(image);
 
         photosDiv.appendChild(link);
